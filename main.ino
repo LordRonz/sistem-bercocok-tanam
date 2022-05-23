47173c84545967f87d3e6a526a489ed097960110
LordRonz <aarontanhar2000@gmail.com>
2022-05-24 00:54:55 +0700

    feat: ngentot lm35 :rocket:

--- main.ino
+++ main.ino
@@ -2,15 +2,14 @@
 #include <Wire.h>
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
+#include <PS2Keyboard.h>
 #include <SPI.h>
-
-#include "constant.h"
 
 // MAX7219
 // Define hardware type, size, and output pins:
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
-#define CS_PIN 3
+#define CS_PIN 10
 
 // LDR
 #define LDR_PIN A0
@@ -18,9 +17,14 @@
 // LM35
 #define LM35_PIN A1
 
+// Keyboard
+#define KB_DATA_PIN 2
+#define KB_IRQ_PIN 3
+
 // Create a new instance of the MD_Parola class with hardware SPI connection:
 MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
+PS2Keyboard keyboard;
 
 DS3231 myRTC;
 
@@ -31,6 +35,20 @@ byte dow;
 byte hour;
 byte minute;
 byte second;
+
+bool century = false;
+bool h12Flag = false;
+bool pmFlag = false;
+
+const byte WAIT = 60;
+
+const String DASH = "-";
+const String SPACE = " ";
+const String COLON = ":";
+const String EMPTY_STR = "";
+
+const char *NAMA = "Aaron Christopher";
+const char *NRP = "07211940000055";
 
 unsigned long timeAlive = 0;
 
@@ -45,16 +63,10 @@ void setup() {
     // Start the serial port
     Serial.begin(9600);
 
+    keyboard.begin(KB_DATA_PIN, KB_IRQ_PIN);
+
     // Start the I2C interface
     Wire.begin();
-
-    myRTC.setYear(0);
-    myRTC.setMonth(0);
-    myRTC.setDate(0);
-    myRTC.setDoW(0);
-    myRTC.setHour(0);
-    myRTC.setMinute(0);
-    myRTC.setSecond(0);
 
     // Initialize the object:
     myDisplay.begin();
@@ -77,19 +89,33 @@ void loop() {
 
     byte curSecond = myRTC.getSecond();
 
-    // Serial.println(getTemp());
-
     if ((curSecond >= 10 && curSecond <= 15) || (curSecond >= 40 && curSecond <= 45)) {
         toBePrinted += getTemp();
     } else {
         toBePrinted += getTime();
     }
 
-    setTemp();
-
     myDisplay.print(toBePrinted);
 
+    if (Serial.available() > 0) {
+        adjustClock(Serial.readString());
+    }
+
     delay(WAIT);
+}
+
+void adjustClock(String data) {
+    byte hour = data.substring(0,2).toInt();
+    byte min = data.substring(3,5).toInt();
+    byte sec = data.substring(6,8).toInt();
+
+    // Example: >> 22:44:35
+
+    myRTC.setHour(hour);
+    myRTC.setMinute(min);
+    myRTC.setSecond(sec);
+
+    Serial.println(F(">> Datetime successfully set!"));
 }
 
 String getTemp() {
